36f2cf1050e731a46619e63abe336b31b1623bc8
Joe Yang <JoeYang82@gmail.com>
2026-03-15 02:28:48 -0500

    oops forgot the /

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/p146',
+  base: '/p146/',
   plugins: [react()],
   build: {
     outDir: "docs"
