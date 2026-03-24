571a8bc5d78d93f7273149e11f9fd886c3cb0caa
Joe Yang <JoeYang82@gmail.com>
2026-03-24 03:03:26 -0500

    basic ui implementaiton

--- src/pages/Chordle.jsx
+++ src/pages/Chordle.jsx
@@ -1,30 +1,67 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { getDocs, collection } from "firebase/firestore";
 import { db } from '../firebase';
-import { Image, Card } from "react-bootstrap";
+import { Image, Form, Button } from "react-bootstrap";
 
 
 export default function Chordle(props) {
     const [notes, setNotes] = useState([]);
+    const guessRef = useRef("");
 
     useEffect(() => {
         getDocs(collection(db, `dailies`))
             .then(snapshot => {
-                setNotes(snapshot.docs.map(doc => 
-                    ({
-                        id: doc.id,
-                        ...doc.data()
-                    })
-                ))
+                const data = snapshot.docs.map(doc =>
+                ({
+                    id: doc.id,
+                    ...doc.data()
+                })
+                )
+                setNotes(data);
+                console.log(data);
             })
     }, []);
 
-    console.log(notes);
+    function getTodayId() {
+        const today = new Date();
 
-    return <div style={{textAlign: "center"}}>
-        <Image 
-        src="/p146/notes/trebleA3.svg" 
-        alt="note A3"
-        style={{height: 300, width: 300}}/>
-    </div>
+        const month = today.getMonth() + 1;
+        const day = today.getDate();
+        const year = today.getFullYear().toString().slice(-2);
+
+        return `${month}-${day}-${year}`;
+    }
+
+    function handleGuess(e) {
+        e.preventDefault();
+        return;
+    }
+
+    const date = getTodayId();
+    const noteToday = notes.find(daily => daily.id === date);
+
+    if (!noteToday) {
+        return <p>Loading... (you just lost the game)</p>
+    } else {
+        return <div style={{ textAlign: "center" }}>
+            <h1>daily or something idk</h1>
+            <Image
+                src={`/p146/notes/${noteToday.name}.svg`}
+                alt={noteToday.description}
+                style={{ height: 500, width: 500 }} />
+
+            <Form onSubmit={handleGuess} style={{ maxWidth: 400, margin: "0 auto" }}>
+                <Form.Label htmlFor="answer">enter a guess</Form.Label>
+                <div style={{display: "flex", flexDirection: "row"}}>
+                    <Form.Control
+                        id="answer"
+                        placeholder="e.g. A4 or A (both work)"
+                        ref={guessRef}
+                    />
+                    <Button type="submit">guess</Button>
+                </div>
+            </Form>
+        </div>
+    }
+
 }
